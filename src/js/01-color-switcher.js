71f083d54aab7e536ca300d984a490d12267d144
MariaKhmel <mariakhmel8@gmail.com>
2022-06-26 04:40:54 +0300

    color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
 
+const refs = {
+    startButton: document.querySelector('button[data-start]'),
+    closeButton: document.querySelector('button[data-stop]'),
+    body : document.querySelector('body'),
+}
+
+let intervalId = null;
+let isButtonClicked = false;
+
+
+refs.startButton.addEventListener('click', onStartButtonClick);
+refs.closeButton.addEventListener('click', onCloseButtonClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+
+function onStartButtonClick () {
+    if (isButtonClicked === true) {
+        return;
+    }
+    isButtonClicked === true;
+    intervalId = setInterval(logger, 1000);
+    
+}
+
+function logger() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+
+function onCloseButtonClick () {
+  
+    clearInterval(intervalId);
+isButtonClicked === false;
+}
+
+
+
+
